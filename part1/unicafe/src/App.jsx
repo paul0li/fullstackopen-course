61dd3f547128b071ad32c040fb2171de3dadd1df
paul0li <paula.jorquera.b@ing.uchile.cl>
2025-02-16 19:22:26 -0300

    Exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,24 +6,32 @@ const Button = (props) => (
 )
 
 
-const Statistics = (props) => (
-
-  <div>
-    <h1>statistics</h1>
-    good {props.good}
-    <br></br>
-    neutral {props.neutral}
-    <br></br>
-    bad {props.bad}
-    <br></br>
-    all {props.good + props.neutral + props.bad}
-    <br></br>
-    average {(props.good - props.bad)/(props.good + props.neutral + props.bad)}
-    <br></br>
-    positive {(props.good/(props.good + props.neutral + props.bad))*100} %
-  </div>
-
-)
+const Statistics = (props) => {
+  if (props.good + props.neutral + props.bad === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        No feedback given
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>statistics</h1>
+      good {props.good}
+      <br></br>
+      neutral {props.neutral}
+      <br></br>
+      bad {props.bad}
+      <br></br>
+      all {props.good + props.neutral + props.bad}
+      <br></br>
+      average {(props.good - props.bad)/(props.good + props.neutral + props.bad)}
+      <br></br>
+      positive {(props.good/(props.good + props.neutral + props.bad))*100} %
+    </div>
+  )
+}
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
